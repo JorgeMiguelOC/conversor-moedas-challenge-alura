12d867498ebfc1d61b5012436f1066002437433d
Jorge Miguel O Costa <jorgemiguel2002@hotmail.com>
2023-07-28 15:50:56 -0300

    Tratamento de exceptions e melhorias no codigo

--- ConversorDeMoedas/src/conversor/TestaConversor.java
+++ ConversorDeMoedas/src/conversor/TestaConversor.java
@@ -5,8 +5,13 @@ public class TestaConversor {
 	public static void main(String[] args) {
 		
 		MenuConversor mc = new MenuConversor();
+		mc.CriaMenu();
+		mc.PegaOpcaoDeMoeda();
 		mc.PegaValor();
-		System.out.println(mc.getValorInputado());
+		System.out.println(mc.getTipoDaMoeda());
+		System.out.println(mc.getTipoDaConversao());
+		System.out.println(mc.getValorParaConversao());
+		
 
 	}
 }
